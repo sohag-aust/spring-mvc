3993435da4e6cceaeeb9a91a6ff505f989108411
sohag-aust <frustratedcoder96@gmail.com>
2022-08-04 14:44:00 +0600

    FormController name Changed

--- src/main/java/controller/ComplexFormController.java
+++ src/main/java/controller/ComplexFormController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class FormController {
+public class ComplexFormController {
 
     @RequestMapping("/complexForm")
     public String showComplexForm() {
